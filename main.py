2763df74ae057a40c82437beb293bafa4f776194
max <s724959099>
2021-01-28 10:38:01 +0800

    chore: temp

---
+++ main.py
@@ -0,0 +1,16 @@
+import justpynew as jp
+import justpy as jp
+
+
+def my_click(self, msg):
+    self.text = 'I was clicked'
+
+
+def event_demo():
+    wp = jp.WebPage()
+    d = jp.Div(text='Not clicked yet', a=wp, classes='w-48 text-xl m-2 p-1 bg-blue-500 text-white')
+    d.on('click', my_click)
+    return wp
+
+
+jp.justpy(event_demo)
