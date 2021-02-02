3318be84c0a91c1d097ef601136f170a673fe2a8
meatball0520 <meatball0520@gmail.com>
2021-02-02 22:51:44 +0800

    chore: add t.py

---
+++ t.py
@@ -0,0 +1,36 @@
+import requests
+import re
+
+
+class SubTitleCralwer:
+
+    def __init__(self, v: str):
+        self.en_url = None
+        self.zh_url = None
+        self.url = f'https://www.youtube.com/watch?v={v}'
+        self.en_subtitles = []
+        self.zh_subtitles = []
+
+    def init(self):
+        self.get_api_urls()
+
+    def get_api_urls(self):
+        url = self.url
+        r = requests.get(url)
+        content = r.text
+        api = re.findall(r'"(https://www.youtube.com/api/timedtext.+?)"', content)[-1]
+        self.en_url = re.sub(r'\\u0026', '&', api)
+        self.zh_url = self.en_url + '&tlang=zh-Hant'
+
+    def get_subtitle(self):
+        r = requests.get(self.en_url)
+        content = r.text
+        re.findall(r'<.+?>(.+?)</text',content)
+        print()
+
+
+if __name__ == '__main__':
+    cralwer = SubTitleCralwer('aoQ6S1a32j8')
+    cralwer.init()
+    cralwer.get_subtitle()
+print()
