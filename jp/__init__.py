756c45fafd4481acbbac2a7efaf0bca102feaf0a
meatball0520 <meatball0520@gmail.com>
2021-02-13 02:48:47 +0800

    feat: increase version to 0.0.2

--- jp/__init__.py
+++ jp/__init__.py
@@ -5,4 +5,4 @@ from .chartcomponents import HighStock
 from .routing import Route, SetRoute
 from .util import run_task, create_delayed_task, try_save
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
